0c646d37e53e97d5b7851ec690146b862cc485d3
Adonya <adonya@gmail.com>
2013-10-27 11:27:49 -0700

    Update main.cpp
    Added 'API' in "m2x api key" and specific which type of keys can be used (e.g. API Key and Master API Key).

--- examples/ReadLocationExample/main.cpp
+++ examples/ReadLocationExample/main.cpp
@@ -5,7 +5,7 @@
 #include "EthernetInterface.h"
 
 char feedId[] = "<feed id>"; // Feed you want to post to
-char m2xKey[] = "<m2x key>"; // Your M2X access key
+char m2xKey[] = "<m2x api key>"; // Your M2X API Key or Master API Key
 
 Client client;
 M2XStreamClient m2xClient(&client, m2xKey, "192.168.1.4", 9393);
