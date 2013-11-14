47e6a936dac63bb6a6535bd48692d9e9cd8b5d0e
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2013-11-14 20:47:04 +0800

    Add example for posting multiple values

---
+++ examples/PostMultiple/main.cpp
@@ -0,0 +1,33 @@
+#include <jsonlite.h>
+#include "M2XStreamClient.h"
+
+#include "mbed.h"
+#include "EthernetInterface.h"
+
+char feedId[] = "<feed id>"; // Feed you want to post to
+char m2xKey[] = "<m2x api key>"; // Your M2X API Key or Master API Key
+
+const char *streamNames[] = { "temperature", "humidity" };
+int counts[] = { 2, 1 };
+const char *ats[] = { "2013-10-11T12:34:56Z", NULL, NULL };
+double values[] = { 7.9, 11.2, 6.1 };
+
+Client client;
+M2XStreamClient m2xClient(&client, m2xKey);
+
+EthernetInterface eth;
+
+int main() {
+  eth.init();
+  eth.connect();
+  printf("IP Address: %s\n", eth.getIPAddress());
+
+  while (true) {
+    int response = m2xClient.postMultiple(feedId, 2, streamNames, counts, ats, values);
+    printf("Post response code: %d\n", response);
+
+    if (response == -1) while (true) ;
+
+    delay(5000);
+  }
+}
