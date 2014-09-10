ce84e43934c673daa1cb496552b551197b48a353
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2014-09-10 20:45:21 +0800

    Add delete values API

---
+++ examples/DeleteValues/main.cpp
@@ -0,0 +1,34 @@
+#include <jsonlite.h>
+#include "M2XStreamClient.h"
+
+#include "mbed.h"
+#include "EthernetInterface.h"
+
+char feedId[] = "<feed id>"; // Feed you want to delete values
+char streamName[] = "<stream name>"; // Stream you want to delete values
+char m2xKey[] = "<M2X access key>"; // Your M2X access key
+
+char fromTime[] = "<from date/time>"; // yyyy-mm-ddTHH:MM:SS.SSSZ
+char endTime[] = "<end date/time>"; // yyyy-mm-ddTHH:MM:SS.SSSZ
+
+Client client;
+M2XStreamClient m2xClient(&client, m2xKey);
+
+EthernetInterface eth;
+
+int main() {
+  eth.init();
+  eth.connect();
+  printf("IP Address: %s\n", eth.getIPAddress());
+
+  // Delete values
+  int response = m2xClient.deleteValues(feedId,
+                                        streamName,
+                                        fromTime,
+                                        endTime);
+  printf("Delete response code: %d\n", response);
+
+  while (true) {
+    delay(5000);
+  }
+}
